f0926a0fd0bd1e8a156ad03cb4c4a6feb5c41504
migel1976 <migel1976@gmail.com>
2024-07-19 10:31:37 +0300

    test redux on example cash state

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+
+const Container=styled.div`
+    display:flex;
+    justify-content: center;
+    gap: 10px;
+`
+
+const App=()=>{
+    const dispatch=useDispatch()
+    const cash=useSelector(state=>state.cash)
+    console.log('cash is', cash)
+
+    const addCash=(cash)=>{
+        dispatch({type:'ADD_CASH', payload:cash})
+    }
+
+    const getCash=(cash)=>{
+        dispatch({type:'GET_CASH', payload:cash})
+    }
+
+    return(
+        <Container>
+            <div style={{fontSize:'5rem'}}>{cash}</div>
+            <button onClick={()=>addCash(Number(prompt('Add cash')))}>Add cash</button>
+            <button onClick={()=>getCash(Number(prompt('Get cash')))}>Get cash</button>
+        </Container>
+    )
+}
+export default App;
