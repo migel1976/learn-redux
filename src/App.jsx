38e25dc807a8f0c4d27a1cd18ecd2ebf99b39544
migel1976 <migel1976@gmail.com>
2024-07-20 10:43:16 +0300

    add customer state, reducer, action

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ const Container=styled.div`
     display:flex;
     justify-content: center;
     gap: 10px;
+    padding-block:10px;
 `
 
 const App=()=>{
     const dispatch=useDispatch()
     const cash=useSelector(state=>state.cash.cash)
-    console.log('cash is', cash)
+    const customers=useSelector(state=>state.customer.customers)
 
     const addCash=(cash)=>{
         dispatch({type:'ADD_CASH', payload:cash})
@@ -20,12 +21,44 @@ const App=()=>{
         dispatch({type:'GET_CASH', payload:cash})
     }
 
+    const addCustomer=(name)=>{
+        const customer={
+            id:Date.now(),
+            name
+        }
+        dispatch({type:'ADD_CUSTOMER', payload:customer})
+    }
+
+    const removeCustomer=(customer)=>{
+        dispatch({type:'REMOVE_CUSTOMER', payload:customer})
+    }
+
     return(
-        <Container>
-            <div style={{fontSize:'5rem'}}>{cash}</div>
-            <button onClick={()=>addCash(Number(prompt('Add cash')))}>Add cash</button>
-            <button onClick={()=>getCash(Number(prompt('Get cash')))}>Get cash</button>
-        </Container>
+        <>
+            <Container>
+                <div style={{fontSize:'5rem'}}>{cash}</div>
+                <button onClick={()=>addCash(Number(prompt('Add cash')))}>Add cash</button>
+                <button onClick={()=>getCash(Number(prompt('Get cash')))}>Get cash</button>
+            </Container>
+            <Container>
+                <button onClick={()=>addCustomer(prompt('Add customer'))}>Add Customer</button>
+                {/* <button onClick={()=>getCustomer(prompt('Get cash')))}>Get Customer</button> */}
+            </Container>
+            {
+                customers.length != 0 ?
+                <div>
+                    {
+                        customers.map(item=><div 
+                            onClick={()=>removeCustomer(item)}
+                            style={{textAlign:'center', border:'1px solid #00f', marginTop:'5px'}} key={item.id}>{item.name}</div>)
+                    }
+                </div>
+                : <div style={{fontSize:'3rem', textAlign:'center', color:'#f00' }}>
+                    There is no clients
+                </div>
+            }
+        
+        </>
     )
 }
 export default App;
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -2,9 +2,13 @@ const defaultState={
     customers:[]
 }
 
-export const customerReducer=(store=defaultState, action)=>{
+export const customerReducer=(state=defaultState, action)=>{
     switch(action.type){
+        case 'ADD_CUSTOMER':
+            return {...state, customers:[ ...state.customers, action.payload ]}
+        case 'REMOVE_CUSTOMER':
+            return {...state, customers:state.customers.filter(item=>item.id !== action.payload.id)}
 
-        default: return store
+        default: return state
     }
 }
